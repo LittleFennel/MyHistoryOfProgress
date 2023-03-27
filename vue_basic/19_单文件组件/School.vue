8e3dbb4a67f6ca2fc9c65df2691de16b7941be8f
hayashisama <kobayashisora@yeah.net>
2023-03-27 21:57:47 +0800

    Vue组件

---
+++ vue_basic/19_单文件组件/School.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class = "demo">
+    <h2>学校名称{{ schoolName }}</h2>
+    <h2>学校地址{{ schoolAddr }}</h2>
+    <button @click="showX">点我提示学校名</button>
+  </div>
+</template>
+
+<script>
+export default ({
+  name: 'School',
+  data() {
+    return {
+      schoolName: '武汉大学',
+      schoolAddr: '珞珈山'
+    }
+  },
+  methods: {
+    showX() {
+      alert(this.schoolName)
+    }
+  },
+})
+</script>
+
+<style lang="style">
+  .demo{
+    background-color: orange;
+  }
+</style>
---
+++ vue_basic/19_单文件组件/Student.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class = "demo">
+    <h2>学生名称{{ studentName }}</h2>
+    <h2>学生年龄{{ studentAge }}</h2>
+    <button @click="showX">点我提示学生名称</button>
+  </div>
+</template>
+
+<script>
+export default ({
+  name: 'Student',
+  data() {
+    return {
+      studentName: '熊昆',
+      studentAge: '24'
+    }
+  },
+  methods: {
+    showX() {
+      alert(this.studentName)
+    }
+  },
+})
+</script>
